fab62f31cabd6670547400eb319b900f844d6edb
nestatbn28 <nestatbn28@gmail.com>
2023-05-04 22:25:15 +0700

    add random virtual account

--- app/Http/Controllers/PembelianController.php
+++ app/Http/Controllers/PembelianController.php
@@ -17,7 +17,16 @@ class PembelianController extends Controller
     public function create()
     {
         $barang = Barang::all();
-        return view('admin.pembelian.create', compact('barang'));
+        $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        // generate a pin based on 2 * 7 digits + a random character
+        $pin = mt_rand(1000000, 9999999)
+            . mt_rand(1000000, 9999999)
+            . $characters[rand(0, strlen($characters) - 1)];
+
+        // shuffle the result
+        $va = str_shuffle($pin);
+        return view('admin.pembelian.create', compact('barang','va'));
     }
 
     public function store(Request $request)
