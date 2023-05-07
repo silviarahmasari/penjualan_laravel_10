fe39aab32bf3538b5ab2aa29fae1e0842da104b4
silviarahma <silviarahmasari02@gmail.com>
2023-05-07 13:42:28 +0700

    add column harga on barang table

---
+++ database/migrations/2023_05_06_142438_add_harga_barang.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('barang', function (Blueprint $table) {
+            $table->bigInteger('harga_beli');
+            $table->bigInteger('harga_jual');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('barang', function (Blueprint $table) {
+            $table->dropColumn('harga_beli');
+            $table->dropColumn('harga_jual');
+        });
+    }
+};
